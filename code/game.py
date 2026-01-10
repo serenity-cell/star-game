17ba2317a4cdacb05b423fe02ac04428debceb00
serenity-cell <justin1726hernandez2@gmail.com>
2026-01-10 18:40:39 -0500

    pictyres and surfaces added

--- code/game.py
+++ code/game.py
@@ -1,30 +1,41 @@
 import pygame
+from os.path import join
+from random import randint
 
 #general setup
 pygame.init
 window_width, window_height = 1800, 1200
 pygame.display.set_caption("asteroid game")
 display = pygame.display.set_mode((window_width, window_height)) 
-
 running = True
 
+#creating the player surface
 surface= pygame.Surface((100,200))
 surface.fill("red")
 x = 100
+
 #importing images
-player_surface = pygame.image.load("images/download(1).jpeg")
+player_surface = pygame.image.load(join("images", "starship.jpeg")).convert()
+star_surface = pygame.image.load(join("images", "stars.png")).convert_alpha()
+star_potisions =[(randint(0, window_width), randint(0, window_height)) for i in range(20)]
+
+#--main loop --
 while running:
     #event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    
 
 
     #draw the game
     display.fill("darkblue")
+    for pos in star_potisions:
+        display.blit(star_surface, pos)
     x += 0.1
-    display.blit(surface, (x,700))
-
+    display.blit(player_surface, (x,700))
+    
     pygame.display.update()
 
 pygame.quit()
