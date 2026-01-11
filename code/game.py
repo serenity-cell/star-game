d29154a78ad677a8ca877662cfc3c89013d57efa
serenity-cell <justin1726hernandez2@gmail.com>
2026-01-11 12:32:28 -0500

    added basic player movement

--- code/game.py
+++ code/game.py
@@ -17,25 +17,42 @@ x = 100
 #importing images
 #importing player img
 player_surface = pygame.image.load(join("images", "ship.png")).convert_alpha()
-player_rect = player_surface.get_rect(center = (window_width/2, window_height/1.2))
+player_rect = player_surface.get_frect(center = (window_width/2, window_height/1.2))
 #importing laser img
 laser_surface = pygame.image.load(join("images", "laser.png")).convert_alpha()
-laser_rect = laser_surface.get_rect(center = (window_width/1.05, window_height/1.1) )
+laser_rect = laser_surface.get_frect(center = (window_width/1.05, window_height/1.1) )
 #importing meteor img
 meteor_surface = pygame.image.load(join("images", "meteor.png")).convert_alpha()
-meteor_rect = meteor_surface.get_rect(center = (window_width/2, window_height/2))
+meteor_rect = meteor_surface.get_frect(center = (window_width/2, window_height/2))
 #importing stars img
 star_surface = pygame.image.load(join("images", "stars.png")).convert_alpha()
 star_potisions =[(randint(0, window_width), randint(0, window_height)) for i in range(20)]
 
 #--main loop --
+def player_movement(window_width, player_rect):
+    keys = pygame.key.get_pressed()
+    if player_rect.right < window_width and player_rect.left > 0:
+        if keys[pygame.K_LEFT]:
+            player_rect.left -= 5
+            print("going left")
+
+        elif keys[pygame.K_RIGHT]:
+            player_rect.left +=  5
+            print("going right")
+            
+    elif player_rect.right < window_width:
+        player_rect.left += 5
+    
+    elif player_rect.left > 0:
+        player_rect.left -= 5
+
 while running:
     #event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    
+            
+    player_movement(window_width, player_rect)
 
     #draw the game
     display.fill("darkblue")
