3839b2fbd8fd3bea128dc12933bbcf1a288e39db
serenity-cell <justin1726hernandez2@gmail.com>
2026-01-12 20:21:11 -0500

    another attempt at player movement: closer, but still failed

--- code/game.py
+++ code/game.py
@@ -19,20 +19,21 @@ x = 100
 player_surface = pygame.image.load(join("images", "ship.png")).convert_alpha()
 player_rect = player_surface.get_frect(center = (window_width/2, window_height/1.2))
 player_direction = pygame.math.Vector2
+player_surface= pygame.transform.scale(player_surface, (100, 200))
 player_speed = 300 
 
 laser_surface = pygame.image.load(join("images", "laser.png")).convert_alpha()
 laser_rect = laser_surface.get_frect(center = (window_width/1.05, window_height/1.1) )
 
 meteor_surface = pygame.image.load(join("images", "meteor.png")).convert_alpha()
-meteor_rect = meteor_surface.get_frect(center = (window_width/2, window_height/2))
+meteor_rect = meteor_surface.get_frect(center = (window_width/2, window_height/1))
 
 
 star_surface = pygame.image.load(join("images", "stars.png")).convert_alpha()
 star_potisions =[(randint(0, window_width), randint(0, window_height)) for i in range(20)]
 
-#player movement like a dvd player 
-'''def player_movement(window_width, player_rect):
+
+"""def player_movement(window_width, player_rect):
     keys = pygame.key.get_pressed()
     if player_rect.right < window_width and player_rect.left > 0:
         if keys[pygame.K_LEFT]:
@@ -47,25 +48,28 @@ star_potisions =[(randint(0, window_width), randint(0, window_height)) for i in 
         player_rect.left += 5
     
     elif player_rect.left > 0:
-        player_rect.left -= 5 '''
+        player_rect.left -= 5  """
 
-def dvd_bounce(window_width, window_height, player_rect):
+#player movement like a dvd player 
+dir_x, dir_y = 2 , 2
+def dvd_bounce(dir_x, dir_y):
     if player_rect.right < window_width and player_rect.left > 0 and player_rect.top < window_height and player_rect.bottom > 0:
-        player_rect.center += player_direction(2, 2) * player_speed * dt
+        player_rect.center += player_direction(dir_x, dir_y) * player_speed * dt
 
-    elif player_rect.top > window_height or player_rect.bottom < 0:
-        player_rect.center += player_direction(2, -2) * player_speed * dt
+    elif player_rect.top < 0 or player_rect.bottom > window_height:      
+        player_rect.bottom -= 5
+        player_rect.center += player_direction(dir_x, -dir_y) * player_speed * dt
 
     elif player_rect.right > window_width and player_rect.left < 0:
-        player_rect.center += player_direction(-2, 2) * player_speed * dt
+        player_rect.center += player_direction(-dir_x, dir_y) * player_speed * dt
         
 
 #-- MAIN LOOP --
 while running:
-    dt = clock.tick(10) / 100000
-    #print(clock.get_fps())
-    print(player_rect.top > window_height and player_rect.bottom <  0)
+    #dynamic fps handling
+    dt = clock.tick(60) / 10000
 
+    print(player_rect.bottom > window_height or player_rect.top < 0 )
     #event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -81,10 +85,9 @@ while running:
     display.blit(laser_surface, laser_rect)
 
     #player moevement
-
-    dvd_bounce(window_width, window_height, player_rect)
-
+    player_surface.fill("orange")
     display.blit(player_surface, player_rect)
+    dvd_bounce(dir_x, dir_y)
 
     pygame.display.update()
 
