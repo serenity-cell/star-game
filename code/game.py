7d43a258d78bbb9f1b72452ead7f4ab759fdb7bc
serenity-cell <justin1726hernandez2@gmail.com>
2026-01-18 10:52:07 -0500

    addition of a small laser timer for later implementation

--- code/game.py
+++ code/game.py
@@ -7,27 +7,44 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image: pygame.Surface = pygame.image.load(join("images", "ship.png")).convert_alpha()
         self.rect: pygame.FRect = self.image.get_frect(center = (window_width/2, window_height/1.2))
-    
-    
-    def update(self, dt):
         self.direction = pygame.math.Vector2()
         self.speed = 500
+        
+        #cooldown
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.cooldown = 400
+    
+    def laser_timer(self):
+        if not self.can_shoot:
+            time = pygame.time.get_ticks()
+            if time - self.shoot_time >= self.cooldown:
+                self.can_shoot = True
+        
+    def update(self, dt):
+        #player movement
         keys = pygame.key.get_pressed()
         self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
         self.direction.x= int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
         self.direction = self.direction.normalize() if self.direction else self.direction
-        if self.rect.top <= 0:
-            self.rect.top += 10
-        elif self.rect.bottom >= window_height:
-            self.rect.bottom -= 10
-        elif self.rect.left <= 0:
-            self.rect.left += 15
-        elif self.rect.right >= window_width:
-           self.rect.right -= 15
-        self.rect.center += self.direction * self.speed * dt
+
+        # window restriction
+        if self.rect.top <= 0 or self.rect.bottom >= window_height:
+            self.direction.y = int(keys[pygame.K_UP]) - int(keys[pygame.K_DOWN])
+        elif self.rect.left <= 0 or self.rect.right >= window_width:
+            self.direction.x= int(keys[pygame.K_LEFT]) - int(keys[pygame.K_RIGHT])
+
         
-        if pygame.key.get_just_pressed()[pygame.K_SPACE]:
+        self.rect.center += self.direction * self.speed * dt
+
+        #laser shot
+        if pygame.key.get_just_pressed()[pygame.K_SPACE] and self.can_shoot:
             print("fire laser")
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+        
+        self.laser_timer()
+        
     
     dir_x, dir_y = 1.5 , 1.5
     def dvd_movement(self, func_y, func_x):
@@ -45,8 +62,7 @@ class Laser(pygame.sprite.Sprite):
         self.image: pygame.Surface = pygame.image.load(join("images", "laser.png")).convert_alpha()
         self.rect: pygame.FRect = self.image.get_frect(center = (window_width/1.05, window_height/1.1) )
     
-    def update(self, dt):
-        pass
+    #def update(self, dt):
 
 class Meteor(pygame.sprite.Sprite):
     def __init__(self, groups):
@@ -86,7 +102,9 @@ meteor = Meteor(all_sprites)
 laser = Laser(all_sprites)
 player =  Player(all_sprites)
 
-
+#meteor event
+meteor_event = pygame.event.custom_type()
+pygame.time.set_timer(meteor_event, 500)
 #-- MAIN LOOP --
 while running:
     #dynamic fps handling
@@ -96,6 +114,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == meteor_event: 
+            print('create meteor')
     
     all_sprites.update(dt)
 
